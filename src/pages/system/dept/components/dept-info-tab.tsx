3be2bbdbc9b79a32170fc6abe1a99f57066232fd
zhizhu <fega.kt@gmail.com>
2026-07-05 09:42:39 +0700

    feat(dept): add form validation for name and code fields using TrimInput

--- src/pages/system/dept/components/dept-info-tab.tsx
+++ src/pages/system/dept/components/dept-info-tab.tsx
@@ -1,9 +1,8 @@
+import { TrimInput } from "#src/components/basic-form";
 import { ProFormDepartmentPicker } from "#src/components/department-picker";
 import { ProFormPeoplePicker } from "#src/components/people-picker";
-import {
-	ProFormRadio,
-	ProFormText,
-} from "@ant-design/pro-components";
+import { ProFormRadio } from "@ant-design/pro-components";
+import { Form } from "antd";
 import { useTranslation } from "react-i18next";
 
 interface DeptInfoTabProps {
@@ -16,21 +15,36 @@ export function DeptInfoTab({ isEditing, excludeRootId }: DeptInfoTabProps) {
 
 	return (
 		<>
-			<ProFormText
-				allowClear
-				rules={[{ required: true }]}
+			<Form.Item
+				required
+				validateFirst
 				name="name"
 				label={t("system.dept.name")}
-				tooltip={t("form.length", { length: 50 })}
-			/>
+				rules={[
+					{ required: true, message: t("system.dept.nameRequired") },
+					{ max: 255, message: t("system.dept.nameMaxLength") },
+					{ pattern: /^\p{L}[\p{L}0-9]*(?:\s[\p{L}0-9]+)*$/u, message: t("system.dept.nameInvalidFormat") },
+				]}
+			>
+				<TrimInput allowClear placeholder={t("common.pleaseInput")} />
+			</Form.Item>
 
-			<ProFormText
-				allowClear
-				rules={[{ required: true }]}
-				disabled={isEditing}
+			<Form.Item
+				required={!isEditing}
+				validateFirst
 				name="code"
 				label={t("system.dept.code")}
-			/>
+				normalize={(v: string) => (v ?? "").toUpperCase()}
+				rules={isEditing
+					? []
+					: [
+						{ required: true, message: t("system.dept.codeRequired") },
+						{ max: 50, message: t("system.dept.codeMaxLength") },
+						{ pattern: /^[A-Z][A-Z0-9]*(?:_[A-Z0-9]+)*$/, message: t("system.dept.codeInvalidFormat") },
+					]}
+			>
+				<TrimInput disabled={isEditing} allowClear={!isEditing} placeholder={t("common.pleaseInput")} />
+			</Form.Item>
 
 			<ProFormDepartmentPicker
 				name="parent"
